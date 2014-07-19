0bffe22c8f560b0b1e6e39c05fe9b4b41a289d46
Dan Evgi <danevgi@gmail.com>
2014-07-19 15:14:35 +0300

    more consts

---
+++ src/bpf/netconsts/archnet.py
@@ -0,0 +1,15 @@
+# RFC 1051 
+ARCTYPE_IP_OLD = 240  # IP protocol
+ARCTYPE_ARP_OLD = 241  # address resolution protocol
+
+# RFC 1201 
+ARCTYPE_IP = 212  # IP protocol
+ARCTYPE_ARP = 213  # address resolution protocol
+ARCTYPE_REVARP = 214  # reverse addr resolution protocol
+
+ARCTYPE_ATALK = 221  # Appletalk
+ARCTYPE_BANIAN = 247  # Banyan Vines
+ARCTYPE_IPX = 250  # Novell IPX
+
+ARCTYPE_INET6 = 0xc4  # IPng
+ARCTYPE_DIAGNOSE = 0x80  # as per ANSI/ATA 878.1
---
+++ src/bpf/netconsts/ipnet.py
@@ -0,0 +1,5 @@
+IPH_AF_INET = 2  # Matches Solaris's AF_INET */
+IPH_AF_INET6 = 26  # Matches Solaris's AF_INET6 */
+
+IPNET_OUTBOUND = 1
+IPNET_INBOUND = 2
